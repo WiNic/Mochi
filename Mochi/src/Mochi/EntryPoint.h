c39545d5fe795ef79cbb9f24b1fda5798fb3eaec
WiNic <65275786+WiNic@users.noreply.github.com>
2023-03-01 22:28:05 +0100

    added premake to project

--- Mochi/src/Mochi/EntryPoint.h
+++ Mochi/src/Mochi/EntryPoint.h
@@ -6,7 +6,9 @@
 extern Mochi::Application* Mochi::CreateApplication();
 
 int main(int argc, char** argv) {
-	
+	Mochi::Log::Init();
+	MOCHI_CORE_WARN("Initialized Log!");
+	MOCHI_INFO("Hello");
 	
 	auto app = Mochi::CreateApplication();
 	app->Run();
---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Mochi"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Mochi"
+    location "Mochi"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "MOCHI_PLATFORM_WINDOWS",
+            "MOCHI_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Engine")
+        }
+    
+    filter "configurations:Debug"
+        defines "MOCHI_DEBUG"
+        symbols "On"
+        
+    filter "configurations:Release" 
+        defines "MOCHI_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "MOCHI_DIST"
+        optimize "On"
+
+
+project "Engine"
+    location "Engine"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "Mochi/vendor/spdlog/include",
+        "Mochi/src"
+    }
+
+    links
+    {
+        "Mochi"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "MOCHI_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "MOCHI_DEBUG"
+        symbols "On"
+        
+    filter "configurations:Release" 
+        defines "MOCHI_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "MOCHI_DIST"
+        optimize "On"
