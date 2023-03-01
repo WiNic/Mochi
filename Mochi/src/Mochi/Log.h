11aa13ed8ae77bd7c77337f0a3d6f808e342bb0f
WiNic <65275786+WiNic@users.noreply.github.com>
2023-03-01 22:28:32 +0100

    added Macros for SPD Log

--- Mochi/src/Mochi/Log.h
+++ Mochi/src/Mochi/Log.h
@@ -21,3 +21,17 @@ namespace Mochi
 }
 
 
+//Core log macros
+#define MOCHI_CORE_ERROR(...) ::Mochi::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define MOCHI_CORE_WARN(...) ::Mochi::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define MOCHI_CORE_INFO(...) ::Mochi::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define MOCHI_CORE_TRACE(...) ::Mochi::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define MOCHI_CORE_FATAL(...) ::Mochi::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+//Client log macros 
+#define MOCHI_ERROR(...) ::Mochi::Log::GetClientLogger()->error(__VA_ARGS__)
+#define MOCHI_WARN(...) ::Mochi::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define MOCHI_INFO(...) ::Mochi::Log::GetClientLogger()->info(__VA_ARGS__)
+#define MOCHI_TRACE(...) ::Mochi::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define MOCHI_FATAL(...) ::Mochi::Log::GetClientLogger()->critical(__VA_ARGS__)
+
